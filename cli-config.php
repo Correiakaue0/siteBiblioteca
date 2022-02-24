ba68f1bf533e9f74f213b2df68ff40900a9f1989
Kaue Correia <correiakaue0@gmail.com>
2022-02-24 10:33:09 -0300

    primeira alteração

--- cli-config.php
+++ cli-config.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-var_dump('ei tbm fiz uma alteração');
+var_dump('fiz uma alteração aqui');
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
     (new \Alura\Cursos\Infra\EntitymanagerCreator())->getEntityManager()
 );
