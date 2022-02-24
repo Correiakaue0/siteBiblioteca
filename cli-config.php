2cfff5f62c47a4d3b6d179a0fbf543452596d7f9
ncadeveloper <ncadeveloper@outlook.com>
2022-02-24 10:11:28 -0300

    Subindo minha alteração

--- cli-config.php
+++ cli-config.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-var_dump('fiz uma alteração aqui');
 
 var_dump('fiz uma alteração aqui');
 
+var_dump('fiz uma alteração aqui');
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
     (new \Alura\Cursos\Infra\EntitymanagerCreator())->getEntityManager()
