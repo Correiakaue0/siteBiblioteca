99a30fab752591a1d7c902a8e6527ca82394b5a7
ncadeveloper <ncadeveloper@outlook.com>
2022-02-24 09:51:04 -0300

    Teste

--- cli-config.php
+++ cli-config.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-
+var_dump('fiz uma alteração');
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
     (new \Alura\Cursos\Infra\EntitymanagerCreator())->getEntityManager()
 );
